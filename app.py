ef7ca06998d4a128c14b874709beab79932c2364
mojtaba <mojtaba@nexalogy.com>
2022-12-02 17:29:52 -0500

    update

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
 import pickle
-from flask import Flask,request,app,jsonfy,url_for,render_template
+from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
+import re
 
 app = Flask(__name__)
 regmodel = pickle.load(open('knnModel.pkl','rb'))
+scalar   = pickle.load(open('norm.pkl','rb'))
 
-@app.rout('/')
+@app.route('/')
 def home():
     return render_template('home.html')
 
 # create predict API
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
-    data = request.json['data']
+    data = request.json["data"]
     print(data)
+    # data = list(data)
 
+    read_df_sample = pd.read_csv(data)
+    features = [i for i in read_df_sample if re.findall("\As\d", i)]
+    features.append('humidity')
+    batch = read_df_sample.loc[read_df_sample['trial_state'] == 'exposure']
+    batch = batch[features]
+    batch = scalar.transform(batch)
+
+    output = regmodel.predict(batch)
+    print(output)
+    return jsonify(output)
+
+if __name__=="__main__":
+    app.run(debug=True)
